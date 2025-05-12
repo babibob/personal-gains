5cb705ccc7b6d518ae429f65252a8f44fb7e31f6
babibob <73898353+babibob@users.noreply.github.com>
2025-05-12 23:23:13 +0400

    Update flux.sh

--- k8s/flux.sh
+++ k8s/flux.sh
@@ -1,6 +1,7 @@
 #
 flux reconcile hr -n $NS helm-release-name --verbose
 
-
 flux suspend hr -n $NS helm-release-name
 flux resume hr -n $NS helm-release-name
+
+flux get hr -n $NS --status-selector ready=false
