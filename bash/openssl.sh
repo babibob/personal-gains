8bc011e4af37d3ed2d8d35a7f8b608b1a738b02f
babibob <73898353+babibob@users.noreply.github.com>
2025-06-04 13:23:38 +0400

    Update openssl.sh

--- bash/openssl.sh
+++ bash/openssl.sh
@@ -3,3 +3,6 @@ echo | openssl s_client -connect example.com:443 2>/dev/null | openssl x509 -noo
 
 # Solted passwword
 openssl passwd -1 -salt My_SaltY0u_must-use_another And_another-paSsW0rd
+
+# The use of printf (as opposed to echo) prevents encoding a newline in the password.
+printf "my_username:my_password" | openssl base64 -A
