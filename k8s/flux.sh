c4e332e5d227c351249ff1107c2b2919a2e9b128
babibob <73898353+babibob@users.noreply.github.com>
2025-07-09 23:11:31 +0400

    Update flux.sh

--- k8s/flux.sh
+++ k8s/flux.sh
@@ -5,3 +5,6 @@ flux suspend hr -n $NS helm-release-name
 flux resume hr -n $NS helm-release-name
 
 flux get hr -n $NS --status-selector ready=false
+
+#Show flux events in kube
+k get events -n flux-system --field-selector type=Warning
