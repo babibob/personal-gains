c005125d7b452fa76f28498d80b1b7de42d36a06
babibob <73898353+babibob@users.noreply.github.com>
2025-07-09 23:22:50 +0400

    Update helm.sh

--- k8s/helm.sh
+++ k8s/helm.sh
@@ -1,5 +1,10 @@
 # Show hr without "Release reconciliation succeeded" 
 k -n $NS get helmreleases.helm.toolkit.fluxcd.io -o wide | grep -v 'Release reconciliation succeeded'
+k get hr -A | grep -i failed
 
 # 
 helm -n $NS history $HR
+
+helm -n $NS ls -a
+
+helm -n $NS get manifest $HR
