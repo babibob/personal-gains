929159c6a8cb280a09d06f1059a6e68c58ead197
babibob <73898353+babibob@users.noreply.github.com>
2025-08-10 13:15:23 +0400

    Update git_hits.sh

--- bash/git_hits.sh
+++ bash/git_hits.sh
@@ -1,13 +1,17 @@
 # Update all git repos
 ls | xargs -P10 -I{} git -C {} pull
+# OR
+find . -type d -name ".git" -execdir sh -c 'echo "#### Updating $(pwd) "####"; git pull' \;
 
 # Clone all gitlab projects;
 # Dependecies - jq git;
 # Generate gitlab token for <TOKEN>;
 # Find gitlab project number for <NUMBER>
-for repo in $(curl -s --header "PRIVATE-TOKEN: <TOKEN>" https://URI/api/v4/groups/<NUMBER> | jq -r ".projects[].web_url"); do git clone $repo; done
+for repo in $(curl -s --header "PRIVATE-TOKEN: <TOKEN>" https://URI/api/v4/groups/<NUMBER> | jq -r ".projects[].web_url"); 
+  do git clone $repo; 
+  done
 
-# Resolve git rebae troubles
+# Resolve git rebase conflicts
 export BRANCH=master ; \
 git checkout ${BRANCH} ; \
 git pull ; \
